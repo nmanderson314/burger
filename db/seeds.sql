cfbb66fa1b8f7f00ccfcd97a975667568b181021
Natalie A <nmanderson314@gmail.com>
2018-10-05 20:42:18 -0400

    initial commits for the config and db folders

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Large Marge", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Jimmy's Page", 1);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Boogan's Bacon", 0);
